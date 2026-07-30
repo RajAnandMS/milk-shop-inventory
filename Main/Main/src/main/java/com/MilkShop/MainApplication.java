eea7fa4fb2fcf5d8f5f72bfa11c3f9bb84f8e5f6
RajAnandMS <rajanand124578@gmail.com>
2026-07-30 12:15:57 +0530

    Add backend test endpoint and mapping logs

--- Main/Main/src/main/java/com/MilkShop/MainApplication.java
+++ Main/Main/src/main/java/com/MilkShop/MainApplication.java
@@ -2,12 +2,19 @@ package com.MilkShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.MilkShop")
+@RestController
 public class MainApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 
+    @GetMapping("/test")
+    public String test() {
+        return "Milk Shop backend is working";
+    }
 }
